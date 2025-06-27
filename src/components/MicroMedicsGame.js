d77384aada9eac1b54c6ba3087380b72c840ff49
EmmanuellaAO <x23282819@student.ncirl.ie>
2025-06-27 06:30:52 +0100

    fix lint errors

--- src/components/MicroMedicsGame.js
+++ src/components/MicroMedicsGame.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useCallback } from 'react';
 import IntroScreen from './screens/IntroScreen';
 import GameScreen from './screens/GameScreen';
